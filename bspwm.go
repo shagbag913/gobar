b4bec4b284f0e8714554ecda3515d989e6a4a6c5
shagbag913 <sh4gbag913@gmail.com>
2020-03-27 23:55:42 -0400

    Get bspwm status straight from socket
    NOTE: this can be improved
    Change-Id: Iba03e9d4fd59f38de57c07fb72e84eb57c5d3528

--- bspwm.go
+++ bspwm.go
@@ -1,25 +1,74 @@
 package main
 
 import (
+    "net"
     "regexp"
-    "os/exec"
     "time"
     "strconv"
     "fmt"
     "os"
+    "io"
 )
+
+var bspwmStatusFromSocket string
+var bspwmReadStatus int
+
+func openBspwmSocket() (net.Conn, error) {
+    sock, err := net.Dial("unix", "/tmp/bspwm_0_0-socket")
+    if err != nil {
+        fmt.Fprintln(os.Stderr, err.Error())
+        return nil, err
+    }
+    return sock, err
+}
+
+func readSocket(reader io.Reader) {
+    buf := make([]byte, 100)
+    num, _ := reader.Read(buf)
+    statusString := string(buf[:num])
+    if statusString != bspwmStatusFromSocket {
+        bspwmStatusFromSocket = statusString
+        bspwmReadStatus = 1
+    } else {
+        bspwmReadStatus = 2
+    }
+}
+
+func setStatusFromSocket() {
+    bspwmReadStatus = 0
+    sock, err := openBspwmSocket()
+    if err != nil {
+        fmt.Fprintln(os.Stderr, err.Error())
+        bspwmReadStatus = 2
+        return
+    }
+    go readSocket(sock)
+
+    msg := []byte("wm\x00--get-status\x00")
+    _, err = sock.Write(msg)
+    if err != nil {
+        fmt.Fprintln(os.Stderr, err.Error())
+        bspwmReadStatus = 2
+        return
+    }
+
+    for {
+        if bspwmReadStatus != 0 {
+            return
+        }
+    }
+}
 
 func setBspwmStatus() {
     reg := regexp.MustCompile("[^oOfF]*")
-
     for {
-        /* TODO: talk directly to socket */
-        socketStatus, err := exec.Command("bspc", "wm", "--get-status").Output()
-        if err != nil {
-            fmt.Fprintln(os.Stderr, err.Error())
-            break
+        setStatusFromSocket()
+        if bspwmReadStatus == 2 {
+            time.Sleep(150 * time.Millisecond)
+            continue
         }
-        socketStatusFormatted := reg.ReplaceAllString(string(socketStatus), "")
+
+        socketStatusFormatted := reg.ReplaceAllString(string(bspwmStatusFromSocket), "")
 
         /* Don't continue if socketStatus hasn't changed */
         if socketStatusFormatted == lastBspwmStatus {
