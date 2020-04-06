cf27d320173f1dc36af98e28b0775072b7e9ea56
shagbag913 <sh4gbag913@gmail.com>
2020-04-06 11:23:54 -0400

    brightness: read straight from sysfs
    Change-Id: I682af253e517a377c6e37ef2a718a17ac66a96d1

--- brightness.go
+++ brightness.go
@@ -1,36 +1,74 @@
 package main
 
 import (
+    "strconv"
     "time"
     "fmt"
     "os"
 )
 
 func setBrightnessString() {
-    volFilePath := os.Getenv("HOME") + "/.cache/brightness/percentage"
-    file, err := os.Open(volFilePath)
+    intelBacklight := "/sys/class/backlight/intel_backlight/"
+    brightnessFile, err := os.Open(intelBacklight + "brightness")
     if err != nil {
         fmt.Fprintln(os.Stderr, err.Error())
         return
     }
-    defer file.Close()
+    defer brightnessFile.Close()
+
+    maxBrightnessFile, err := os.Open(intelBacklight + "max_brightness")
+    if err != nil {
+        fmt.Fprintln(os.Stderr, err.Error())
+        return
+    }
+
+    maxBrightnessRaw := make([]byte, 4)
+    var num int
+    num, err = maxBrightnessFile.Read(maxBrightnessRaw)
+    if err != nil {
+        fmt.Fprintln(os.Stderr, err.Error())
+        return
+    }
+    maxBrightnessFile.Close()
+
+    var maxBrightness int
+    maxBrightness, err = strconv.Atoi(string(maxBrightnessRaw[:num-1]))
+    if err != nil {
+        fmt.Fprintln(os.Stderr, err.Error())
+        return
+    }
 
     for {
-        /* Fetch brightness from cache file */
-        _, err := file.Seek(0, 0)
-        if err != nil {
-            fmt.Fprintln(os.Stderr, err.Error())
-            break
-        }
-        brightnessPercentageFromFile := make([]byte, 3)
-        var num int
-        num, err = file.Read(brightnessPercentageFromFile)
+        _, err = brightnessFile.Seek(0, 0)
         if err != nil {
             fmt.Fprintln(os.Stderr, err.Error())
             break
         }
 
-        newBrightnessString := " " + string(brightnessPercentageFromFile[:num]) + "%"
+        brightnessRaw := make([]byte, 4)
+        num, err = brightnessFile.Read(brightnessRaw)
+        if err != nil {
+            fmt.Fprintln(os.Stderr, err.Error())
+            break
+        }
+        var brightnessInt int
+        brightnessInt, err = strconv.Atoi(string(brightnessRaw[:num-1]))
+        if err != nil {
+            fmt.Fprintln(os.Stderr, err.Error())
+            break
+        }
+        brightnessDiv := float32(brightnessInt) / float32(maxBrightness)
+        brightnessPercentage := ""
+        if brightnessDiv != 1 {
+            brightnessPercentage = fmt.Sprintf("%.2f", brightnessDiv)[2:]
+        } else {
+            brightnessPercentage = "100"
+        }
+        /* Remove beginning 0 from percentage string */
+        if brightnessPercentage[0] == '0' {
+            brightnessPercentage = brightnessPercentage[1:]
+        }
+        newBrightnessString := " " + brightnessPercentage + "%"
         if newBrightnessString != brightnessString {
             brightnessString = newBrightnessString
             printBuffer()
