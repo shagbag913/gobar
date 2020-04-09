deda6557c86ab2d2f135db9b8dba3f93eb168a6a
shagbag913 <sh4gbag913@gmail.com>
2020-04-09 00:15:54 -0400

    Support configurable status string locations
    Change-Id: I48766b34769fd4de5538819176fa52d29b82d0fd

--- gobar.go
+++ gobar.go
@@ -18,6 +18,9 @@ var memoryString string
 /* Left, center, and right print buffers */
 var buffers = [3]string{"%{l}", "%{c}", "%{r}"}
 
+/* List of modules to enable for left, center, and right */
+var enabledModules [3]string
+
 /* Buffer element seperator */
 var elementSeperator = "   |   "
 
@@ -34,11 +37,16 @@ func main() {
         "used_memory": setMemoryString,
     }
 
-    enabledModules := getConfValue("main;enabled_modules")
+    enabledModules[0] = getConfValue("main;modules_left")
+    enabledModules[1] = getConfValue("main;modules_center")
+    enabledModules[2] = getConfValue("main;modules_right")
 
     for moduleString, moduleFunction := range moduleMap {
-        if enabledModules == "" || strings.Contains(enabledModules, moduleString) {
-            go moduleFunction.(func())()
+        for _, moduleSide := range enabledModules {
+            if strings.Contains(moduleSide, moduleString) {
+                go moduleFunction.(func())()
+                break
+            }
         }
     }
 
@@ -53,6 +61,17 @@ func addToBuffer(element string, bufferIndex int) {
 }
 
 func printBuffer() {
+    moduleNameMap := map[string]string {
+        "time": timeString,
+        "charge": chargeString,
+        "bspwm": bspwmStatus,
+        "net": netStatus,
+        "date": dateString,
+        "volume": volumeString,
+        "brightness": brightnessString,
+        "used_memory": memoryString,
+    }
+
     printBuffer := ""
 
     /* clear buffers */
@@ -60,25 +79,26 @@ func printBuffer() {
         buffers[i] = buffers[i][:4]
     }
 
-    addToBuffer(bspwmStatus, 0)
-    addToBuffer(timeString, 1)
-    addToBuffer(dateString, 1)
-    addToBuffer(memoryString, 2)
-    addToBuffer(brightnessString, 2)
-    addToBuffer(volumeString, 2)
-    addToBuffer(netStatus, 2)
-    addToBuffer(chargeString, 2)
+    for side, enabledModulesSide := range enabledModules {
+        for _, module := range strings.Split(enabledModulesSide, ",") {
+            addToBuffer(moduleNameMap[module], side)
+        }
+    }
 
+    /*
+     * A space is left between buffers to prevent status strings containing
+     * percentage signs from conflicting from the side identifier
+     */
     if buffers[0] != "%{l}" {
-        printBuffer += buffers[0][:len(buffers[0])-len(elementSeperator)]
+        printBuffer += buffers[0][:len(buffers[0])-len(elementSeperator)+1]
     }
 
     if buffers[1] != "%{c}" {
-        printBuffer += buffers[1][:len(buffers[1])-len(elementSeperator)]
+        printBuffer += buffers[1][:len(buffers[1])-len(elementSeperator)+1]
     }
 
     if buffers[2] != "%{r}" {
-        printBuffer += buffers[2][:len(buffers[2])-len(elementSeperator)]
+        printBuffer += buffers[2][:len(buffers[2])-len(elementSeperator)+1]
     }
 
     if printBuffer != "" {
